d3c1b80bc3badd181c3aa08ab57976ded4f4b1a1
janewysoki <jane.wysoki@gmail.com>
2021-01-22 15:03:14 -0500

    add validations to flashcard class

--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -1,4 +1,8 @@
 class Flashcard < ApplicationRecord
   belongs_to :user
   belongs_to :study_guide
+
+  validates :cardfront, presence: true
+  validates :cardback, presence: true
+  validates :memorized, inclusion: { in: [true, false] }
 end
